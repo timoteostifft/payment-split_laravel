0309f8925e7d7a3dee9d43705b1dbfac45e69249
Timoteo stifft <timoteostifft@gmail.com>
2022-02-21 21:36:09 -0300

    feature: set home page route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\CompanyController@list')->name('company_form');
+
+Route::post('/', 'App\Http\Controllers\CompanyController@list')->name('save_company_form');
