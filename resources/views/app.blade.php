e1613c54c6d7bee313d77915159bc7e5b56213d8
Timoteo stifft <timoteostifft@gmail.com>
2022-02-21 21:35:45 -0300

    feature: set display/hide mode for companies form

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <link rel="stylesheet" href="{{asset(asset('css/app.css'))}}">
- <title>Payment Split</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset(asset('css/app.css'))}}">
+  <title>Payment Split</title>
 </head>
 <body>
   <main>
@@ -14,10 +14,26 @@
     
       <div id="header">
         <h5>COMPANIES:</h5>
-        <button>ADD</button>
+        <button onclick='handleChangeCompanyFormVisibility()'>ADD</button>
       </div>
-
+      
+        <div id='companyForm'>
+          @yield('company_form')
+        </div>
+      
     </div>
   </main>
+  <script>
+    
+    function handleChangeCompanyFormVisibility(){
+      var companyForm = document.getElementById('companyForm')
+      
+      if (companyForm.style.display === 'none'){
+        companyForm.style.display = 'block';
+      } else {
+        companyForm.style.display = 'none';
+      }
+    }
+  </script>
 </body>
-</html></h1>
+</html>
