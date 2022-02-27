ecbd34dcf28a90b624ac0ac24f43c729723c3962
Timoteo stifft <timoteostifft@gmail.com>
2022-02-26 23:01:17 -0300

    refactor: ajust code to clean code

--- resources/views/companies/list.blade.php
+++ resources/views/companies/list.blade.php
@@ -2,14 +2,14 @@
   <br>
   @foreach ($companiesList as $company)
     <div id='listData'>
-      <p>Name: {{ $company->Name }}</p>
-      <p>CPNJ: {{ $company->CNPJ }}</p>
-      <p>Amount: {{ $company->Amount }}</p>
+      <p>Name: {{ $company->name }}</p>
+      <p>CPNJ: {{ $company->cnpj }}</p>
+      <p>Amount: {{ $company->amount }}</p>
 
       <div id="buttons">
         <button>LIST</button>
         <button>
-          <a href="{{route('delete_company', $company->ID)}}">REMOVE</a>
+          <a href="{{route('delete_company', $company->id)}}">REMOVE</a>
         </button>
       </div>
     </div>
