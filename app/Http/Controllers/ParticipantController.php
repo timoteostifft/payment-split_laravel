19b255fe2e8929ca040193d4b781c84f608f60f7
Timoteo stifft <timoteostifft@gmail.com>
2022-02-26 23:25:57 -0300

    feature: create participant controller

---
+++ app/Http/Controllers/ParticipantController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ParticipantController extends Controller
+{
+    public function list(){
+
+        return view('participants.list');
+        
+        // $data = Participant::list();
+
+        // return view('app',['companiesList' => $data]);
+    }
+}
