29ca30e6bb131c17eb23027c8b1e64c251c032d3
Timoteo stifft <timoteostifft@gmail.com>
2022-02-28 14:16:06 -0300

    feature: create list participant form layout

--- resources/views/participants/list.blade.php
+++ resources/views/participants/list.blade.php
@@ -1,3 +1,30 @@
-<div>
-  <h1>HERE</h1>
+<header>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</header>
+
+<div id="participantsPage">
+  <h3>PAYMENT SPLIT</h3>
+
+  <div id="participantsPageHeader">
+    <h5>PARTICIPANTS FROM $COMPANY</h5>
+    <button onclick="handleChangeParticipantFormVisibility()" id="handleParticipantForm">ADD</button>
+  </div>
+  
+  <div id="participantsForm" style="display: none">
+    @include('participants.add')
+  </div>
 </div>
+
+<script>
+  function handleChangeParticipantFormVisibility(){
+    var participantForm = document.getElementById('participantsForm')
+      
+    if (participantForm.style.display === 'none'){
+      participantForm.style.display = 'block';
+      document.getElementById('handleParticipantForm').innerHTML = 'CLOSE'
+    } else {
+      participantForm.style.display = 'none';
+      document.getElementById('handleParticipantForm').innerHTML = 'ADD'
+    }
+  }
+</script>
