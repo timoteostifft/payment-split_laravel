9aa817eb809968580a8b0fc4be9405ad1417a806
Timoteo stifft <timoteostifft@gmail.com>
2022-03-01 20:08:23 -0300

    feature: set participants add controller method

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -3,21 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Participant;
+use App\Models\CompanyParticipant;
 class ParticipantController extends Controller
 {
-    public function list(){
+    public function list($companyId){
 
-        return view('participants.list');
-        
-        // $data = Participant::list();
-
-        // return view('app',['companiesList' => $data]);
+        return view('participants.list', ['companyId' => $companyId]);
     }
 
-    public function add(){
+    public function add(Request $request){
 
-        return view('participants.list');
+        $validated = $request->validate([
+            'name' => 'required',
+            'cnpj' => 'required',
+            'percent' => 'required'
+            ]);
+
+        Participant::add($validated);
+        
+        $participantId = Participant::getId();
+
+        CompanyParticipant::add($request->route('id'),$participantId);
+
+        return view('participants.list', ['companyId' => $request->route('id')]);
     }
 
 }
