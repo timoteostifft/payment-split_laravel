b59667a04795f26dcac9c7332d6c53772e146b58
Timoteo stifft <timoteostifft@gmail.com>
2022-03-02 13:56:35 -0300

    feature: set list relation between participants and company method

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -6,15 +6,21 @@ use Illuminate\Http\Request;
 use App\Models\Participant;
 use App\Models\Company;
 use App\Models\CompanyParticipant;
+use DB;
 class ParticipantController extends Controller
 {
     public function list($companyId){
 
         $companyName = Company::getName($companyId);
 
+        $data = DB::select('select * from participants, companies_participants
+        where ? = companies_participants.company_id and participants.id = companies_participants.participant_id
+        ',[$companyId]);
+
         return view('participants.list', [
             'companyId' => $companyId,
-            'companyName' => strtoupper($companyName)
+            'companyName' => strtoupper($companyName),
+            'data' => $data
         ]);
     }
 
@@ -32,11 +38,16 @@ class ParticipantController extends Controller
 
         CompanyParticipant::add($request->route('id'),$participantId);
 
-        $companyName = Company::getName($companyId);
+        $companyName = Company::getName($request->route('id'));
+
+        $data = DB::select('select * from participants, companies_participants
+        where ? = companies_participants.company_id and participants.id = companies_participants.participant_id
+        ',[$request->route('id')]);
 
         return view('participants.list', [
             'companyId' => $request->route('id'),
-            'companyName' => strtoupper($companyName)
+            'companyName' => strtoupper($companyName),
+            'data' => $data
         ]);
     }
 
