2c42ec4413fe14c848d0a157aed19dc2c334c822
Timoteo stifft <timoteostifft@gmail.com>
2022-03-07 03:49:04 -0300

    feature: set business rules for participant controller add method

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -24,19 +24,41 @@ class ParticipantController extends Controller
 
     public function add(Request $request){
 
+        $company = Company::get($request->route('id'));
+
         $validated = $request->validate([
             'name' => 'required',
             'cnpj' => 'required',
             'percent' => 'required'
             ]);
 
-        Participant::add($validated);
-        
-        $participant = Participant::get();
+        $data = Participant::list($request->route('id'));
 
-        CompanyParticipant::add($request->route('id'),$participant->id);
+        $participantTotalPercent = 0;
 
-        return redirect()->route('listParticipants', ['id' => $request->route('id')]);
+        foreach ($data as $participant){
+         $participantTotalPercent = $participantTotalPercent + $participant->percent;
+        }
+
+        $participantTotalPercent = $participantTotalPercent + $request['percent'];
+
+        if ($participantTotalPercent > 0){
+            if ($participantTotalPercent < 100){
+                Participant::add($validated);
+                $participant = Participant::get();
+                CompanyParticipant::add($request->route('id'),$participant->id);
+                return redirect()->route('listParticipants', ['id' => $request->route('id')]);
+            }
+        }
+
+        $message = 'INVALID SPLIT, VERIFY PARTICIPANTS PERCENTS.';
+
+        return view('participants.list', [
+            'companyId' => $company->id,
+            'companyName' => strtoupper($company->name),
+            'data' => $data,
+            'message' => $message
+        ]);
 
     }
 
